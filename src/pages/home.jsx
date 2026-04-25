02038c4e6d4376d4c16f745a074e739f05b342d0
Aquib Ali <nitinshaukia@gmail.com>
2026-04-25 06:53:09 +0530

    Update QR code image import path

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './home.css';
-import qrCodeImage from '../assest/qr-code.png';
+import qrCodeImage from '../assest/qr-code (1).png';
 import hotelLogo from '../assest/logo.jpg';
 
 export default function ReviewHomePage() {
